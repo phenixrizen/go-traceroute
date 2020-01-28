14f74dc421b92bd14bdf19b281cca4455b0932a7
Nathan Rockhold <nathan@netlify.com>
2020-01-27 17:32:49 -0800

    added go mod and struct JSON tags

--- tracedata.go
+++ tracedata.go
@@ -7,22 +7,22 @@ import (
 
 // TraceData represents data received by executing traceroute.
 type TraceData struct {
-	Hops    [][]Hop
-	Dest    net.IP
-	Timeout time.Duration
-	Tries   int
-	MaxTTL  int
-	Port    int
-	Proto   string
-	IPv     string
+	Hops    [][]Hop       `json:"hops,omitempty"`
+	Dest    net.IP        `json:"dest,omitempty"`
+	Timeout time.Duration `json:"timeout,omitempty"`
+	Tries   int           `json:"tries,omitempty"`
+	MaxTTL  int           `json:"max_ttl,omitempty"`
+	Port    int           `json:"port,omitempty"`
+	Proto   string        `json:"proto,omitempty"`
+	IPv     string        `json:"i_pv,omitempty"`
 }
 
 // Hop represents a path between a source and a destination.
 type Hop struct {
-	TryNumber int
-	TTL       int
-	AddrIP    net.IP
-	AddrDNS   []string //net.IPAddr
-	Latency   time.Duration
-	Err       error
+	TryNumber int           `json:"try_numberomitempty"`
+	TTL       int           `json:"ttl,omitempty"`
+	AddrIP    net.IP        `json:"addr_ip,omitempty"`
+	AddrDNS   []string      `json:"addr_dns,omitempty"` //net.IPAddr
+	Latency   time.Duration `json:"latency,omitempty"`
+	Err       error         `json:"err,omitempty"`
 }
